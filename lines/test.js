6279f68deb411933fdf4acae3ab9fc8963f7f9c4
JP <49571924+jtheletter@users.noreply.github.com>
2022-05-21 19:30:58 -0700

    Reset test lines.

--- lines/test.js
+++ lines/test.js
@@ -1,17 +1,2 @@
-module.exports = ['‎ח‎ו‎,‎ ‎H‎e‎b‎r‎o‎w‎.‎','\u200Eח\u200Eו\u200E,\u200E Hebraw.\u200E',
-'test 2',
-'test 3',
-'test 4',
-'test 5',
-'test 6',
-'test 7',
-'test 8',
-'test 9',
-'test 10',
-'test 11',
-'test 12',
-'test 13',
-'test 14',
-'test 15',
-'test 16',
+module.exports = [
 ];
